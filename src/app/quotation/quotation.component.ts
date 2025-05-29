510ff678bc73d2ac07e584e6aa7f6eff56b139c0
rahulyadav-2003 <rulydv2003@gmail.com>
2025-05-30 04:02:17 +0530

    working on the quotation

--- src/app/quotation/quotation.component.ts
+++ src/app/quotation/quotation.component.ts
@@ -7,9 +7,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotationComponent implements OnInit {
 
+
+   mail : string = 'sales@stcoverseas.co.in';
+   quotationForm : boolean = true;
+    sameAsBilled: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+quotationDetails = {
+   billedTo: {
+      name: '',
+      address: '',
+      phone: ''
+    },
+    shipTo: {
+      name: '',
+      address: '',
+      phone: ''
+    },
+  clientName: '',
+  clientAddress: '',
+  clientPhone: '',
+  contactPerson: '',
+  validityDate: '',
+  referenceFile: null,
+  items: [
+    { description: '', qty: null, unitPrice: null }
+  ],
+  additionalCharges: 0,
+  terms: ''
+};
+
+
+ copyBilledToShip() {
+    if (this.sameAsBilled) {
+      this.quotationDetails.shipTo = { ...this.quotationDetails.billedTo };
+    } else {
+      this.quotationDetails.shipTo = { name: '', address: '', phone: '' };
+    }
+  }
+
+
+addItem() {
+  this.quotationDetails.items.push({ description: '', qty: null, unitPrice: null });
+}
+
+removeItem(index: number) {
+  if (this.quotationDetails.items.length > 1) {
+    this.quotationDetails.items.splice(index, 1);
+  }
+}
+
+
+submitForm(){
+
+}
+
+  
+
+
+   items = [
+    { description: 'Item 1', hsn: '4564', qty: 3, rate: 50, amount: 150 },
+    { description: 'Item 2', hsn: '4154', qty: 2, rate: 100, amount: 200 },
+    { description: 'Item 3', hsn: '7878', qty: 3, rate: 200, amount: 600 }
+  ];
 
 }
